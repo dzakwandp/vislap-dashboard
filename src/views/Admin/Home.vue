f2097537ad20a02dc4dc575c64355ddfd82d5527
Dzakwan Diego <diegodzakwan@gmail.com>
2023-09-08 03:05:25 +0700

    add admin home page

--- src/views/Admin/Home.vue
+++ src/views/Admin/Home.vue
@@ -1,31 +1,126 @@
 <template lang="">
-  <div>
-      <p>This is home</p>
-  </div>
+  <v-container>
+    <v-card class="mb-4" color="blue-darken-2">
+      <v-card-title> Total Transaksi <v-icon>mdi-cash</v-icon> </v-card-title>
+      <v-progress-linear
+        v-if="transLoad"
+        :indeterminate="true"
+        color="blue-darken-4"
+      ></v-progress-linear>
+      <v-card-title v-else>
+        {{ transData.length }}
+      </v-card-title>
+    </v-card>
+    <v-card class="text-white mb-4" color="orange darken-2">
+      <v-card-title> Total Service <v-icon>mdi-tools</v-icon> </v-card-title>
+      <v-progress-linear
+        v-if="serviceLoad"
+        :indeterminate="true"
+        color="orange-darken-4"
+      ></v-progress-linear>
+      <v-card-title v-else>
+        {{ serviceData.length }}
+      </v-card-title>
+    </v-card>
+    <v-card color="green-darken-1">
+      <v-card-title>
+        Barang hampir habis
+        <v-btn
+          class="ml-4 text-body-2"
+          variant="outlined"
+          @click="this.$router.push('/products')"
+          >Manage</v-btn
+        ></v-card-title
+      >
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-title color="green-darken-1">
+            Show
+          </v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <v-table class="text-body-2">
+              <thead>
+                <tr>
+                  <th>Barang</th>
+                  <th>Stock</th>
+                </tr>
+              </thead>
+              <tbody v-for="item in productData">
+                <tr v-if="item.stock < 10">
+                  <td>{{ item.nama }}</td>
+                  <td>{{ item.stock }}</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+  </v-container>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 
-import { useAuthStore } from '@/store/authStore';
-import { useEnvStore } from '@/store/envStore';
+import { useAuthStore } from "@/store/authStore";
+import { useEnvStore } from "@/store/envStore";
 export default {
-  methods: {
-    async getAdminInfo(){
-      try{
-        const profile=await axios.get(useEnvStore().apiUrl+'admins/profile',{
-          headers:{
-            Authorization:'Bearer '+useAuthStore().accessToken
-          }
-        })
-        console.log(profile)
-      }
-      catch(err){
-        console.log(err)
-      }
-    }
+  data() {
+    return {
+      transData: [],
+      serviceData: [],
+      productData: [],
+      transLoad: true,
+      serviceLoad: true,
+    };
   },
-}
+  methods: {
+    async getTrans() {
+      try {
+        const trans = await axios.get(useEnvStore().apiUrl + "txs", {
+          headers: {
+            Authorization: "Bearer " + useAuthStore().accessToken,
+          },
+        });
+        this.transData = trans.data;
+        this.transLoad = false;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async getService() {
+      try {
+        const service = await axios.get(
+          useEnvStore().apiUrl + "services/admin",
+          {
+            headers: {
+              Authorization: "Bearer " + useAuthStore().accessToken,
+            },
+          }
+        );
+        this.serviceData = service.data;
+        this.serviceLoad = false;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async getProduct() {
+      try {
+        const prod = await axios.get(useEnvStore().apiUrl + "products", {
+          headers: {
+            Authorization: "Bearer " + useAuthStore().accessToken,
+          },
+        });
+        this.productData = prod.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  mounted() {
+    this.getService();
+    this.getTrans();
+    this.getProduct();
+  },
+};
 </script>
-<style lang="">
-  
-</style>
+<style lang=""></style>
